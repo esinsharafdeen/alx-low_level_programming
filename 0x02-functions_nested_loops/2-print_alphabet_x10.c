236a33c1425421049d4bf399282f146a1cc7cc21
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-13 11:50:26 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,18 @@
 
 void print_alphabet_x10(void)
 {
-	int alphabet;
+	char alphabet;
 	int count;
 
-	count = 0;
-	while (count < 10)
+	while (count <= 9)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
 
-		count++;
 		_putchar('\n');
+
+		count++;
 	}
-	return (0);
 }
