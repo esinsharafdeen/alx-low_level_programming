273bcfbe6a2a349717560cca98d02d77f111b065
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-15 06:09:16 -0700

    Task 0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,3 +8,19 @@
 int main(void)
 {
 	int i;
+
+	printf("Infinte loop incoming:(\n");
+
+	i = 0;
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("Infinit loop avoided!\\o/\n");
+
+	return (0);
+}
