f0868ae42f83e949f5427bb3d1ba31162783a97f
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-16 04:09:22 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,15 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		if (n != 2 && n != 4)
 		{
 			_putchar(n);
+		}
+		else
+		{
+			continue;
 		}
 	}
 	_putchar('\n');
