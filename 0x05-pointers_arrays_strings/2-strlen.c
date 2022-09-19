913d7b9ea98ec35a19c7f43b6fe850a0adf54796
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-19 08:49:04 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - prints length of a string
+ * @s: parameter
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	strl;
+	*s[] = "Software";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		strl = putchar("%d\n", i);
+	}
+	return (strl);
+}
+
