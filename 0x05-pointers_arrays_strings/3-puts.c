c59a1d2164791bc86b41a23d89c526f115066db1
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-19 13:31:43 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string and a new line
+ * @str: pointer to be printed
+ * Return: Nothing
+ */
+
+void _puts(chr *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
