9f722aec0e2e022a41c83faab3f405ddde60ef93
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-19 15:28:44 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - prints elements of an array of intege
+ * @a: parameter one
+ * @n: parameter two
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--; i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf('\n');
+}
