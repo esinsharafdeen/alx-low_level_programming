1add35f9fa59bfb8e9057bd32f506493153f53ee
esinsharafdeen <esinsharafdeen@gmail.com>
2022-09-19 03:41:47 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,13 +9,5 @@
 
 void reset_to_98(int *n)
 {
-	int p;
-
-	p = 98;
-	n = &p;
-
-	_putchar("Value of 'p' is: %d\n", p);
-	_putchar("Address of 'p' is: %p\n", &p);
-	_putchar("Value of 'n' is: %p\n", n);
-	_putchar("Address of 'n' is: %p\n", &n);
+	*n = 98;
 }
