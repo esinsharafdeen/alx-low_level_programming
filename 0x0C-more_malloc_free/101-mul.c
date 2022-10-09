a88b93e84a6d10a9ddd8fd5af41bed75cf56f96a
esinsharafdeen <esinsharafdeen@gmail.com>
2022-10-09 14:22:31 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-
-int dind_len(char *str);
+int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
-coid get_prod(char *prod, char *mult, int digit, int zeroes);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
 
 /**
  * find_len - Finds the length of a string.
  * @str: The string to be measured.
+ *
  * Return: The length of the string.
  */
-
 int find_len(char *str)
 {
 	int len = 0;
@@ -23,16 +22,17 @@ int find_len(char *str)
 		len++;
 
 	return (len);
-
+}
 
 /**
- * create_xarray - Creates an array if chars and initializesnit with the character 'x'. Adds a terminating null byte
+ * create_xarray - Creates an array of chars and initializes it with
+ *                 the character 'x'. Adds a terminating null byte.
  * @size: The size of the array to be initialized.
  *
- * Desription: If there is insufficient space, the function exits with a status of 98
- * Return: A pointer to the array
+ * Description: If there is insufficient space, the
+ *              function exits with a status of 98.
+ * Return: A pointer to the array.
  */
-
 char *create_xarray(int size)
 {
 	char *array;
@@ -45,17 +45,19 @@ char *create_xarray(int size)
 
 	for (index = 0; index < (size - 1); index++)
 		array[index] = 'x';
+
 	array[index] = '\0';
 
 	return (array);
 }
 
 /**
- * Iterate_zeroes - Iterates through a string of numbers containing leading zeroes unril it hits a non-zero number.
- * @str: The string of numbers to be iterated through.
- * Return: A pointer to the next non-zero element
+ * iterate_zeroes - Iterates through a string of numbers containing
+ *                  leading zeroes until it hits a non-zero number.
+ * @str: The string of numbers to be iterate through.
+ *
+ * Return: A pointer to the next non-zero element.
  */
-
 char *iterate_zeroes(char *str)
 {
 	while (*str && *str == '0')
@@ -65,12 +67,13 @@ char *iterate_zeroes(char *str)
 }
 
 /**
- * get_digit - Xonverts a digit character to a corresponding int.
+ * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
- * Description: If c is a non-digit, the function exits with a status of 98
+ *
+ * Description: If c is a non-digit, the function
+ *              exits with a status of 98.
  * Return: The converted int.
  */
-
 int get_digit(char c)
 {
 	int digit = c - '0';
@@ -89,11 +92,11 @@ int get_digit(char c)
  * @prod: The buffer to store the result.
  * @mult: The string of numbers.
  * @digit: The single digit.
- * @zeroes: The necessary number of leading zeroes
+ * @zeroes: The necessary number of leading zeroes.
  *
- * Description: If mult vontains a non-digit, the functiin exits with a status value of 98.
+ * Description: If mult contains a non-digit, the function
+ *              exits with a status value of 98.
  */
-
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
 	int mult_len, num, tens = 0;
@@ -102,19 +105,6 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	mult += mult_len;
 
 	while (*prod)
-	{
-		*prod = 'x';
-		prod++;
-	}
-	prod--;
-
-	while (zeroes--)
-	{
-		*prod = '0';
-		prod--;
-	}
-
-	for (; mult_len >= 0; mult_len--, mult--, prod--)
 	{
 		*prod = 'x';
 		prod++;
@@ -147,15 +137,14 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 }
 
 /**
- * add_nums - Adds the numbers stored in two strings
- * @final_ prod: The buffer storing the running final product.
+ * add_nums - Adds the numbers stored in two strings.
+ * @final_prod: The buffer storing the running final product.
  * @next_prod: The next product to be added.
  * @next_len: The length of next_prod.
  */
-
-void add _nums(char *final_prod, char *next_prod, int next_len)
+void add_nums(char *final_prod, char *next_prod, int next_len)
 {
-	int num, ten = 0;
+	int num, tens = 0;
 
 	while (*(final_prod + 1))
 		final_prod++;
@@ -163,7 +152,7 @@ void add _nums(char *final_prod, char *next_prod, int next_len)
 	while (*(next_prod + 1))
 		next_prod++;
 
-	for(; *final_prod != 'x'; final_prod--)
+	for (; *final_prod != 'x'; final_prod--)
 	{
 		num = (*final_prod - '0') + (*next_prod - '0');
 		num += tens;
@@ -174,7 +163,7 @@ void add _nums(char *final_prod, char *next_prod, int next_len)
 		next_len--;
 	}
 
-	for (; nezt_len >= 0 && *next_prod != 'x'; next_len--)
+	for (; next_len >= 0 && *next_prod != 'x'; next_len--)
 	{
 		num = (*next_prod - '0');
 		num += tens;
@@ -190,4 +179,54 @@ void add _nums(char *final_prod, char *next_prod, int next_len)
 }
 
 /**
+ * main - Multiplies two positive numbers.
+ * @argv: The number of arguments passed to the program.
+ * @argc: An array of pointers to the arguments.
  *
+ * Description: If the number of arguments is incorrect or one number
+ *              contains non-digits, the function exits with a status of 98.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *final_prod, *next_prod;
+	int size, index, digit, zeroes = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (*(argv[1]) == '0')
+		argv[1] = iterate_zeroes(argv[1]);
+	if (*(argv[2]) == '0')
+		argv[2] = iterate_zeroes(argv[2]);
+	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	size = find_len(argv[1]) + find_len(argv[2]);
+	final_prod = create_xarray(size + 1);
+	next_prod = create_xarray(size + 1);
+
+	for (index = find_len(argv[2]) - 1; index >= 0; index--)
+	{
+		digit = get_digit(*(argv[2] + index));
+		get_prod(next_prod, argv[1], digit, zeroes++);
+		add_nums(final_prod, next_prod, size - 1);
+	}
+	for (index = 0; final_prod[index]; index++)
+	{
+		if (final_prod[index] != 'x')
+			putchar(final_prod[index]);
+	}
+	putchar('\n');
+
+	free(next_prod);
+	free(final_prod);
+
+	return (0);
+}
